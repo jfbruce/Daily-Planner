dc129918b6d4ea7b5dc16742242e8c81f3484971
jfbruce <117041959+jfbruce@users.noreply.github.com>
2022-12-11 22:07:32 -0800

    Added html JS and CSS
    added files with basic framework on html

---
+++ Assets/Script.js
@@ -0,0 +1,17 @@
+// current date
+var today = dayjs();
+$("#date").text(today.format("dddd MMMM D"));
+
+
+
+// addEventListener("click")
+
+// // variables 
+
+// var hour-suffix = ":00 AM";
+// var storedblocks = [];
+
+        
+        // var hour-text = 9;
+        // S("#time").text(hour-text)
+
